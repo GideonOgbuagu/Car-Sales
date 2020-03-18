b56d5d8daf5587fe30ff84f17cf11a9f63e189e5
Gideon <gideon@Mac-1s-MBP.home>
2020-03-18 08:30:27 -0400

    Store is Set up and connect() is initialized for App.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { salesReducer } from './reducers/salesReducer';
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+const store = createStore(salesReducer);
+// store is Set up
+
+console.log(store.getState())
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+</Provider>
+, rootElement);
+
+    
